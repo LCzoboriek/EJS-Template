86f1a557bd277dec88c31fc339d1a0d30e5b4cc4
Luke Czoboriek <LC169782@protonmail.com>
2022-03-24 15:27:12 +0000

    think this is working now on pulling the myUser object through

--- editprofile.js
+++ editprofile.js
@@ -3,26 +3,8 @@ const user = require('./user.js');
 const myTest = require('./restController')
 
 
-function processEditProfile(params, res, myUser){
-    // let myQuery = `SELECT password, first_name, id, isadmin, last_name FROM "${tableName}" WHERE ${columnName} = '${userName}'`;
-    // client.query(myQuery, 
-    //     (error, result) => {
-    //         if(error){
-    //             console.log(error);
-    //         } else {
-    //             myUser.setid = result.rows[0].id;
-    //             myUser.setfirstname = result.rows[0].first_name;
-    //             myUser.setlastname = result.rows[0].last_name;
-    //             myUser.setpassword = dbpassword;
-    //             console.table(myUser);
-    //             res.render('welcome', {firstname: myUser.getfirstname, lastname: myUser.getlastname});
-    //             } else {
-    //                 res.render('login',{message: 'Invalid username or password'});
-    //             }
-    //         }
-    console.log(myTest.myUser);
-    console.table(myTest.myUser);
-    res.render('editprofile', {username: myUser.getusername, firstname: myUser.getfirstname, lastname: myUser.getlastname, message: ""})
+function processEditProfile(params, res){
+    res.render('editprofile', {username: myTest.myUser.getusername, firstname: myTest.myUser.getfirstname, lastname: myTest.myUser.getlastname, message: ""})
 }
 
 
