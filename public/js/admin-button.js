8d2e23125a9cad9ae13962bc95106ae1c1dee69b
Luke Czoboriek <LC169782@protonmail.com>
2022-03-28 11:58:51 +0100

    admin now works

--- public/js/admin-button.js
+++ public/js/admin-button.js
@@ -1,7 +1,10 @@
-let isadmin = document.getElementById('admin-button').value
 
+
+let isadmin = document.getElementById('admin-button').value
+alert(isadmin)
 if(isadmin === 'Y') {
     document.getElementById('admintoggle').style.display = 'block'
 } else {
     document.getElementById('admintoggle').style.display = 'none'
 }
+
